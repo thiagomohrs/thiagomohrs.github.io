d11c74d132539db938b14f3042ff9767ec0b96ee
Thiago Mohr da Silveira <thiagomohrs@gmail.com>
2016-05-21 12:10:16 -0300

    ajustando id's

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,4 @@
-String.prototype.replaceAll = String.prototype.replaceAll || function(needle, replacement) {
+gString.prototype.replaceAll = String.prototype.replaceAll || function(needle, replacement) {
   return this.split(needle).join(replacement);
 };
 var totalLinhas = 2;
@@ -27,21 +27,16 @@ function replaceAll(str, needle, replacement) {
   return str.split(needle).join(replacement);
 }
 
-function replace_all(string, encontrar, substituir) {
-  while (string.indexOf(encontrar) >= 0)
-    string = string.replace(encontrar, substituir);
-  return string;
-}
 
 function myFunction() {
   //TODO Lógica do programa
-  var NT1 = document.getElementById('NT1');
-  var NT2 = document.getElementById('NT2');
-  var NT1_1 = document.getElementById('NT1_1');
-  var NT2_1 = document.getElementById('NT2_1');
+  var row0col0 = document.getElementById('row0col0');
+  var row1col0 = document.getElementById('row1col0');
+  var row0col1 = document.getElementById('row0col1');
+  var row1col1 = document.getElementById('row1col1');
   var resultadosSemNT = new Array;
-  var aposReplace = NT1_1.value.replaceAll(NT1.value, NT1_1.value);
-  aposReplace = aposReplace.replaceAll(NT2.value, NT2_1.value);
+  var aposReplace = row0col1.value.replaceAll(row0col0.value, row0col1.value);
+  aposReplace = aposReplace.replaceAll(row1col0.value, row1col1.value);
 
   if (tem_apenas_minusculas(aposReplace)) {
     resultadosSemNT.push(aposReplace);
@@ -56,10 +51,13 @@ function myFunction() {
 var numerolinhas = 2
 
 function AddTableRow() {
+  var numerocolunas = 0;
   var newRow = $("<tr>");
   var cols = "";
-  cols += '<td><input type="text" id="NT2"> -></td>';
-  cols += '<td><input type="text" id="NT2_1"></td>';
+  cols += '<td><input type="text" class="campo" id=\"row' + numerolinhas + '\col' + numerocolunas + '"\> -></td>';
+  numerocolunas++;
+  cols += '<td><input type="text" class="campo" id=\"row' + numerolinhas + '\col' + numerocolunas + '"\></td>';
+  numerolinhas++;
   cols += '<td><button onclick="AddTableCol(this)" type="button" class="btn">+</button></td>';
   cols += '<td><button onclick="RemoveTableRow(this)" type="button" class="btn">-</button></td>';
   newRow.append(cols);
@@ -84,7 +82,7 @@ function RemoveTableRow(linha) {
     return;
   }
   var tr = $(linha).closest('tr');
-  tr.fadeOut(400, function() {
+  tr.fadeOut(100, function() {
     tr.remove();
   });
   totalLinhas--;
